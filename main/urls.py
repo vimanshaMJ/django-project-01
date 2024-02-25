32f97165cfdac7d515e43eb81465962d4db6eb19
vimansha <vimanshamadushani88@gmail.com>
2024-02-25 12:27:25 +0530

    add <int:id> to urls.py and change the views.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<int:id>", views.index, name="index"),
 ]
+
+# <int:id> : we're gonna look for some integer in our path and we're gonna pass that to the function views.index
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Tech with vimansha!</h1>")
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" %id)
 
 
