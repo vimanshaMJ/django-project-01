7ad3c57eb74c541431a509cda975c610fa23d2e8
vimansha <vimanshamadushani88@gmail.com>
2024-02-25 12:39:26 +0530

    show the item text

--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from .models import ToDoList, Item
 # Create your views here.
 def index(response, name):
     ls = ToDoList.objects.get(name = name)
-    return HttpResponse("<h1>%s</h1>" % ls.name)
+    item = ls.item_set.get(id = 1)
+    return HttpResponse("<h1> %s </h1> <br> <p> %s </p>" % (ls.name, str(item.text)))
 
 
