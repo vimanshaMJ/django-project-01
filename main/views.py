3fe5e40f9bcb9ada027b13ce682853ed66961754
vimansha <vimanshamadushani88@gmail.com>
2024-02-25 12:32:37 +0530

    show ToDoList names

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import ToDoList, Item
+
 # Create your views here.
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+    ls = ToDoList.objects.get(id = id)
+    return HttpResponse("<h1>%s</h1>" % ls.name)
 
 
