503858c7a9af0f8032b86519d22de7efc3c8f403
vimansha <vimanshamadushani88@gmail.com>
2024-02-26 19:36:50 +0530

    register Item as well

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ToDoList
+from .models import ToDoList, Item
 
 # Register your models here.
 admin.site.register(ToDoList)
+admin.site.register(Item)
