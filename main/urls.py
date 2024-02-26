e36c48356bcf93f9c0e639438f89c92d48385c13
vimansha <vimanshamadushani88@gmail.com>
2024-02-26 19:34:40 +0530

    update ToDoList to site

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,3 @@ urlpatterns = [
     path("<str:name>", views.index, name="index"),
 ]
 
-# <int:id> : we're gonna look for some integer in our path and we're gonna pass that to the function views.index
--- main/views.py
+++ main/views.py
@@ -10,14 +10,3 @@ def index(response, name):
     return HttpResponse("<h1> %s </h1> <br> <p> %s </p>" % (ls.name, str(item.text)))
 
 
-# >>> del_object = t.get(id=1)
-# >>> del_object.delete()
-# (2, {'main.Item': 1, 'main.ToDoList': 1})
-# >>> t.filter(id=2)
-# <QuerySet []>
-# >>> t.all()
-# <QuerySet []>
-# >>> t1 = ToDoList(name="First List")
-# >>> t1.save()
-# >>> t2 = ToDoList(name="Second List")
-# >>> t2.save()
