343e834b26a64dfb96c0106d68f4427f5df589b3
vimansha <vimanshamadushani88@gmail.com>
2024-02-28 11:15:12 +0530

    check box make as not required

--- main/form.py
+++ main/form.py
@@ -2,5 +2,4 @@ from django import forms
 
 class CreateNewList(forms.Form):
     name = forms.CharField(label="name", max_length=200)
-    check = forms.BooleanField()
-    
+    check = forms.BooleanField(required = False)
