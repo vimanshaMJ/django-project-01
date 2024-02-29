21b0d79acd002b1a7ec2446301cfb86f81b0fd6b
vimansha <vimanshamadushani88@gmail.com>
2024-02-29 20:06:16 +0530

    fix the return

--- main/views.py
+++ main/views.py
@@ -30,6 +30,7 @@ def index(response, id):
             else:
                 print("Invalid")
             
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {})
