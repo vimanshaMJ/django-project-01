2f41d5f21d159b60d25cc07db66a4b724e40df71
vimansha <vimanshamadushani88@gmail.com>
2024-03-01 06:55:26 +0530

    added RegisterForm

--- register/views.py
+++ register/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render, redirect
-
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm  #UserCreationForm-used to create new user
+from .forms import RegisterForm
 
 # Create your views here.
 def register(response):
     if response.method == "POST":
-        form = UserCreationForm(response.POST)
+        form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()  #saves the user in the user's database
 
         return redirect("/home")
     
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(response, "register/base.html", {"form": form})
