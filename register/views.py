5af45de201a8fcde6e095c19634652c8fb394752
vimansha <vimanshamadushani88@gmail.com>
2024-03-01 06:11:05 +0530

    render the form

--- register/views.py
+++ register/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm  #UserCreationForm-used to create new user
+
 # Create your views here.
 def register(response):
-    return render()
+    form = UserCreationForm()
+    return render(response, "register/base.html", {"form": form})
