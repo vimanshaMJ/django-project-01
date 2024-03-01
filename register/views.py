12a1dfce70768440be444665912cfdf251d25dd9
vimansha <vimanshamadushani88@gmail.com>
2024-03-01 06:36:23 +0530

    use conditions & redirect

--- register/views.py
+++ register/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm  #UserCreationForm-used to create new user
 
 # Create your views here.
 def register(response):
-    form = UserCreationForm()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()  #saves the user in the user's database
+
+        return redirect("/home")
+    
+    else:
+        form = UserCreationForm()
+
     return render(response, "register/base.html", {"form": form})
